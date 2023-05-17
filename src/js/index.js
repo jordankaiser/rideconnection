4b46ae97dc6504ac83ebaaa1c760120e20bebac0
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2023-05-17 09:11:46 -0500

    Add comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import { gsap } from "gsap";
 
+// Add animation to submenus.
 const headerSubmenus = document.querySelectorAll('.header ul li ul');
-
 headerSubmenus.forEach((submenu) => {
   const submenuTimeline = gsap.timeline({
     paused: true,
@@ -11,6 +11,7 @@ headerSubmenus.forEach((submenu) => {
     onReverseCompleteParams: [submenu],
   });
 
+  // Add animation to the submenu.
   submenuTimeline.fromTo(submenu, {
     opacity: 0,
     duration: 1
@@ -18,24 +19,28 @@ headerSubmenus.forEach((submenu) => {
     opacity: 1,
   });
 
+  // Add animation to the submenu element.
   submenu.animation = submenuTimeline;
 });
 
+// Set the display to flex when the animation starts.
 function showSubmenu(submenu) {
   submenu.style.display = 'flex';
 };
 
+// Set the display to none when the animation ends.
 function hideSubmenu(submenu) {
   submenu.style.display = 'none';
 };
 
+// Add event listeners to the header links.
 const headerLinks = document.querySelectorAll('.header > .container > ul > li');
-
 headerLinks.forEach((link) => {
   link.addEventListener('mouseenter', () => {
     const submenu = link.querySelector('ul');
 
     if (submenu) {
+      // Play the animation.
       submenu.animation.play();
     }
   });
@@ -43,6 +48,7 @@ headerLinks.forEach((link) => {
     const submenu = link.querySelector('ul');
 
     if (submenu) {
+      // Reverse the animation.
       submenu.animation.reverse();
     }
   });
